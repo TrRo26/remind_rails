1662cba22e0696619e0c0d9124ef529010bfdf91
Vince Roti <vince.roti@gmail.com>
2016-12-16 13:59:10 -0600

    basic rails app setup

---
+++ db/migrate/20161216172432_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :lists do |t|
+      t.string :name, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
+  end
+end
