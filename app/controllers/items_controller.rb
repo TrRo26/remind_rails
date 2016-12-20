6fbcde0a59b4c160e061a11b5d022f1dc9c1b884
Vince Roti <vince.roti@gmail.com>
2016-12-20 10:19:58 -0600

    made controller for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,59 @@
 class ItemsController < ApplicationController
 
-  def create
-    @item = Item.create(item_params)
+  def show
+    item = Item.find(params[:id])
+    render json: {item: item.name, items: item.items }, status: 201
   end
+
+  def index
+    items = Item.all
+    items_content = []
+    items.each do |item|
+      item_hash = {
+        name: item.name,
+        list_id: item.list_id
+      }
+      items_content << item_hash
+    end
+    render json: { items: items_content }, status: 201
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item.to_json
+    else
+      render json: { message: "Invalid Input" }, status: 422
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update_attributes(item_params)
+    if item.save
+      render json: item.to_json
+    else
+      render json: { message: "Invalid Input" }, status: 422
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      render json: { message: "item Deleted"}
+    else
+      render json: { message: "item does not exist" }, status: 404
+    end
+
+  end
+
 
   private
 
   def item_params
-    params.require(:item).permit(:list_id, :name)
+    params.require(:item).permit(:name, :user_id)
   end
+
 end
+
+
