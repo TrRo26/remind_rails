1b80af99d86882693e7ed0710880973e6d817dd9
Vince Roti <vince.roti@gmail.com>
2016-12-21 11:29:43 -0600

    comment out auth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def authenticate_request
-    @current_user = AuthorizeApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
-  end
+  # def authenticate_request
+  #   @current_user = AuthorizeApiRequest.call(request.headers).result
+  #   render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  # end
 
 end
-
-
